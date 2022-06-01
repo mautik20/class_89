c75e10a51ea56ce33d8879a7aa12390a4ac8fa6a
mautik20 <73740190+mautik20@users.noreply.github.com>
2022-06-01 20:32:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDRih8YFWOpKsBQ0RErwbSDHOEH01MeEc4",
+  authDomain: "story-telling-app-3114f.firebaseapp.com",
+  databaseURL: "https://story-telling-app-3114f-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-3114f",
+  storageBucket: "story-telling-app-3114f.appspot.com",
+  messagingSenderId: "26242200305",
+  appId: "1:26242200305:web:17d39453b8cee30eb7ef50"
+};
